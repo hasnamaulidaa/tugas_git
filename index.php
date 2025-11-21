52d0196560e6b4aeb1bcdc7dc5585614597bbe39
milik qiyaa <tasqiyaa28@gmail.com>
2025-11-21 12:23:23 +0700

    Melalukan perubahan nama

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "Mari belajar Github!";
-$nama = "Hasna Maulida & intan tasqiya";
+$nama = "Hasna Maulida (23.N1.0013) & intan tasqiya (23.N1.0027)";
 echo "<br>Halo, " . $nama . "!";
 ?>
